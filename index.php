6624bd66919108f43ae98ce12efecc0b07f3d0c2
David Truesdale <davidt1978@gmail.com>
2020-12-01 20:10:48 -0500

    After Refactoring

--- index.php
+++ index.php
@@ -16,87 +16,49 @@
 
       <main>
         <?php
-    $nameFile = fopen("asgn12-names.txt", "r");
-    $nameRecord = fgets($nameFile);
-    $fullNameArray = array();
-    $firstNameArray = array();
-    $lastNameArray = array();
-    $lastNameCount = array();
-    $firstNameCount = array();
-    
-    while (!feof($nameFile)) {
-      if (substr($nameRecord, 0, 1) != " " and is_numeric(substr($nameRecord, 0, 1)) != 1) {
-        list ($lName, $fName) = explode(" ", $nameRecord);
-        $lName = substr($lName, 0, -1);
-              
-        $fullNameArray[] = "$lName$fName";
-        $firstNameArray[] = "$fName";
-        $lastNameArray[] = "$lName";
-      }
-      $nameRecord = fgets($nameFile);
-    }
-    
-    fclose($nameFile);
-    
-    $namesCount = count($fullNameArray);
-    $namesCount = number_format($namesCount);
-    print("<p>The total number of names is $namesCount.");
-    
-    $uniqueFullNameArray = array_unique($fullNameArray);
-    $uniqueFirstNameArray = array_unique($firstNameArray);
-    $uniqueLastNameArray = array_unique($lastNameArray);
-    
-    $fullNameCount = count($uniqueFullNameArray);
-    $firstNameCount = count($uniqueFirstNameArray);
-    $lastNameCount = count($uniqueLastNameArray);   
-    
-    $fullNameCount = number_format($fullNameCount);
-    $fistNameCount = number_format($firstNameCount);
-    $lastNameCount = number_format($lastNameCount);
-    
+    /*** Standard Statistics ***/
+    print("<p>The total number of names is $namesCount.</p>");
     print("<p>The number of unique full names is $fullNameCount.</p>");    
     print("<p>The number of unique first names is $firstNameCount.</p>");    
     print("<p>The number of unique last names is $lastNameCount.</p>");
-    
-   
-    $lastNameCount = array_count_values($lastNameArray);
-    asort($lastNameCount);
+  
+    /*** Top Ten Last Names ***/
     print("<p>The top last names are as follows.</p>");
     print("<ol>");
     $c = 0;
     while ($c < 10) {
       if ($c == 0) {
-        $lNameCountOne = end($lastNameCount);
-        $lNameOne = key($lastNameCount);
+        $lNameCountOne = end($lastNameCountArray);
+        $lNameOne = key($lastNameCountArray);
         print("<li>$lNameOne: $lNameCountOne</li>");
       } else {
-        $lNameCountOne = prev($lastNameCount);
-        $lNameOne = key($lastNameCount);
+        $lNameCountOne = prev($lastNameCountArray);
+        $lNameOne = key($lastNameCountArray);
         print("<li>$lNameOne: $lNameCountOne</li>");
       }
       $c++;
     }
     print("</ol>");
-    
-    $firstNameCount = array_count_values($firstNameArray);
-    asort($firstNameCount);
+  
+    /*** Top Ten First Names ***/
     print("<p>The top first names are as follows.</p>");
     print("<ol>");
     $c = 0;
     while ($c < 10) {
       if ($c == 0) {
-        $fNameCountOne = end($firstNameCount);
-        $fNameOne = key($firstNameCount);
+        $fNameCountOne = end($firstNameCountArray);
+        $fNameOne = key($firstNameCountArray);
         print("<li>$fNameOne: $fNameCountOne</li>");
       } else {
-        $fNameCountOne = prev($firstNameCount);
-        $fNameOne = key($firstNameCount);
+        $fNameCountOne = prev($firstNameCountArray);
+        $fNameOne = key($firstNameCountArray);
         print("<li>$fNameOne: $fNameCountOne</li>");
       }
       $c++;
     }
     print("</ol>");
     
+    /*** Unique Full Names ***/
     $i = 0;
     print("<p>25 unique name from the data set are as follows.");
     print("<ol>");
@@ -114,6 +76,7 @@
     }
       print("</ol>");
     
+    /*** Modified Unique Full Names ***/
     $n = 0;
     print("<p>25 modified unique name from the data set are as follows.");
     print("<ol>");
--- php/my-functions.php
+++ php/my-functions.php
@@ -14,6 +14,50 @@ function head_info() {
 
 // Name Functions
 
+// Remove names from file and remove names that begin with a number.
 
+$nameFile = fopen("asgn12-names.txt", "r");
+$nameRecord = fgets($nameFile);
+
+while (!feof($nameFile)) {
+  if (substr($nameRecord, 0, 1) != " " and is_numeric(substr($nameRecord, 0, 1)) != 1) {
+    list ($lName, $fName) = explode(" ", $nameRecord);
+    $lName = substr($lName, 0, -1);
+
+    $fullNameArray[] = "$lName$fName";
+    $firstNameArray[] = "$fName";
+    $lastNameArray[] = "$lName";
+  }
+  $nameRecord = fgets($nameFile);
+}
+
+fclose($nameFile);
+
+// Full count of all the names left.
+
+$namesCount = count($fullNameArray);
+$namesCount = number_format($namesCount);
+
+// Pull out only the unique values, count them, and format the count for all three arrays.
+
+$uniqueFullNameArray = array_unique($fullNameArray);
+$uniqueFirstNameArray = array_unique($firstNameArray);
+$uniqueLastNameArray = array_unique($lastNameArray);
+
+$fullNameCount = count($uniqueFullNameArray);
+$firstNameCount = count($uniqueFirstNameArray);
+$lastNameCount = count($uniqueLastNameArray);   
+
+$fullNameCount = number_format($fullNameCount);
+$fistNameCount = number_format($firstNameCount);
+$lastNameCount = number_format($lastNameCount);
+
+// Create sorted array of first and last names with counts.
+
+$lastNameCountArray = array_count_values($lastNameArray);
+asort($lastNameCountArray);
+
+$firstNameCountArray = array_count_values($firstNameArray);
+asort($firstNameCountArray);
 
 // EOF
